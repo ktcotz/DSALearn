d4a025095a984123a099166cbd89c910319a658c
ktcotz <naskret.kamil@gmail.com>
2024-08-13 05:38:28 +0200

    feat: Searching Algorithms
    Work with Binary Search & Naive Search.

--- learn/SearchingAlgorithms.md
+++ learn/SearchingAlgorithms.md
@@ -14,3 +14,13 @@ Searching oznacza termin przeszukiwania pewnego rodzaju danych na podstawie tego
 ## Linear Search
 
 Iterujemy po każdym elemencie i sprawdzamy czy warunek się sprawdza dla danego elementu.
+
+## Binary Search
+
+Dzieli na połowy i sprawdza czy element jest już znaleziony, zwraca go, jeżeli nie to rusza sie w strone mniejszą lub większą.
+
+Binary Search działa tylko na posortowanych tablicach!
+
+## Naive Search
+
+Pozwala wyszukać podciąg w ciągu.
--- src/searching/searching.ts
+++ src/searching/searching.ts
@@ -12,3 +12,43 @@ linearSearch([100], 100); // 0
 linearSearch([1, 2, 3, 4, 5], 6); // -1
 linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10); // -1
 linearSearch([100], 200); // -1
+
+const binearSearch = (numbers: number[], target: number) => {
+  let start = 0;
+  let end = numbers.length - 1;
+
+  while (start <= end) {
+    let index = Math.floor((start + end) / 2);
+    let middle = numbers[index];
+
+    if (middle === target) return index;
+    if (middle > target) end = index - 1;
+    if (middle < target) start = index + 1;
+  }
+
+  return -1;
+};
+
+binearSearch([10, 15, 20, 25, 30], 15); // 1
+binearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4); // 5
+binearSearch([100], 100); // 0
+binearSearch([1, 2, 3, 4, 5], 6); // -1
+binearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10); // -1
+binearSearch([100], 200); // -1
+
+const naiveSearch = (str: string, search: string) => {
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    let temp = "";
+    for (let j = 0; j < search.length; j++) {
+      if (str[j] !== str[i + j]) break;
+      temp += search[j];
+    }
+    if (temp === search) sum++;
+    temp = "";
+  }
+
+  return sum;
+};
+
+console.log(naiveSearch("omgblablaomg", "omg"));
