bc2d8ca776f5c750c060bab62da672313be78c99
ktcotz <naskret.kamil@gmail.com>
2024-10-28 16:44:05 +0100

    feat: search-insert-position
    Write a search-insert-position, binary search
    implementation.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { SinglyLinkedList } from "./data-structures/singly-linked-list/singly-linked-list";
 import { strStr } from "./leetcode-problems/find-the-index-of-the-first-occurence-in-a-string";
+import { searchInsert } from "./leetcode-problems/search-insert-position";
 
 const sll = new SinglyLinkedList();
 
 sll.push(35);
 sll.push(500);
 
-console.log(strStr("sadbutsad", "sad"));
-console.log(strStr("leetcode", "leeto"));
-console.log(strStr("hello", "ll"));
-console.log(strStr("mississippi", "issip"));
+searchInsert([1, 3, 5, 6], 5);
+searchInsert([1, 3, 5, 6], 2);
+searchInsert([1, 3, 5, 6], 7);
