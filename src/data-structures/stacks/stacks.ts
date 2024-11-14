9366e2aad630e4c47adc2d2938a5657f4b6752ef
ktcotz <naskret.kamil@gmail.com>
2024-11-14 21:32:02 +0100

    feat: linked stack
    Working on the linked stack implementation with JSDoc.

--- src/data-structures/stacks/stacks.ts
+++ src/data-structures/stacks/stacks.ts
@@ -22,6 +22,55 @@ export class ArrayStack<T> {
  * Stack implemented with linked list
  */
 
+class StackNode<T> {
+  constructor(public value: T, public next: StackNode<T> | null = null) {}
+}
+
 export class LinkedListStack<T> {
-  constructor(public stack: T[] = []) {}
+  constructor(
+    public first: StackNode<T> | null = null,
+    public last: StackNode<T> | null = null,
+    public size: number = 0
+  ) {}
+
+  /**
+   *
+   * @param value
+   */
+  push(value: T) {
+    const newNode = new StackNode(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    this.size++;
+
+    return this.size;
+  }
+
+  /**
+   * Remove the first node from the stack
+   */
+  pop() {
+    if (!this.first) return null;
+
+    const temp = this.first;
+
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+
+    this.size--;
+
+    return temp.value;
+  }
 }
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { DoublyLinkedList } from "./data-structures/doubly-linked-list/doubly-linked-list";
-
-const dll = new DoublyLinkedList();
-
-dll.push(35);
-dll.push(55);
-
-console.log(dll.get(1));
