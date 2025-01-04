1aec086a81e74f557333b5982cba3ee66b21b91f
ktcotz <naskret.kamil@gmail.com>
2025-01-04 14:49:23 +0100

    feat: longest-substring
    Work around longest-substring solution with own
    challenge approach.

--- src/leetcode-problems/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ src/leetcode-problems/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,4 +1,48 @@
-/* BRUTE FORCE */
-/* OPTIMAL WINDOW SOLUTION */
+/*
+  My Solution Approach : 
+  const temp: string[] = [];
+  let length = 0;
 
-export function lengthOfLongestSubstring(s: string): number {}
+  for (let i = 0; i < s.length; i++) {
+    const isInsideTemp = temp.includes(s[i]);
+
+    if (!isInsideTemp) {
+      temp.push(s[i]);
+      length = Math.max(length, temp.length);
+    }
+
+    if (isInsideTemp) {
+      length = Math.max(length, temp.length);
+
+      const firstIndexOfAppearance = temp.indexOf(s[i]);
+
+      temp.splice(0, firstIndexOfAppearance + 1);
+      temp.push(s[i]);
+    }
+  }
+
+  return length;
+
+*/
+
+export function lengthOfLongestSubstring(s: string): number {
+  /* Optimal Approach */
+
+  const seen: Record<string, number> = {};
+  let start = 0;
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    maxLength = Math.max(maxLength, i - start + 1);
+
+    seen[char] = i + 1;
+  }
+
+  return maxLength;
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { lengthOfLongestSubstring } from "./leetcode-problems/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters";
+
+// lengthOfLongestSubstring("ohvhjdml");
+// lengthOfLongestSubstring("abcabcbb");
+// lengthOfLongestSubstring("bbbbb");
+lengthOfLongestSubstring("pwwkew");
+// lengthOfLongestSubstring("dvdf");
+// lengthOfLongestSubstring("aabaab!bb");
