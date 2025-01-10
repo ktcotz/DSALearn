c14d8c5cd9c715fe11795ff0a22bc4648fea9ef4
ktcotz <naskret.kamil@gmail.com>
2025-01-10 17:02:54 +0100

    feat: codewars daily
    Work around daily codewars challenges.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,1 @@
-import { countPrefixSuffixPairs } from "./leetcode-problems/count-prefix-and-suffix-pairs-1/count-prefix-and-suffix-pairs-1";
-
-countPrefixSuffixPairs(["a", "aba", "ababa", "aa"]);
-countPrefixSuffixPairs(["pa", "papa", "ma", "mama"]);
-countPrefixSuffixPairs(["abab", "ab"]);
+import "./codewars/learning-typescript-mixins";
